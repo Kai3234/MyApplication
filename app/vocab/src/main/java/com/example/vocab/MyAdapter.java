2cec95578b4ff10f6b1637c05f8903ec71850887
Kai3234 <khai2892005@gmail.com>
2026-03-04 10:43:58 +0700

    Vocab

---
+++ app/vocab/src/main/java/com/example/vocab/MyAdapter.java
@@ -0,0 +1,88 @@
+package com.example.vocab;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+
+    private Context context;
+    private ArrayList<Vocab> data;
+
+
+    // Constructor để khởi tạo Adapter với dữ liệu và Context
+    public MyAdapter(Context context, ArrayList<Vocab> data) {
+        this.context = context;
+        this.data = data;
+    }
+
+
+    // Tạo ViewHolder cho mỗi mục trong danh sách
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_list, parent, false);
+        return new ViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull MyAdapter.ViewHolder holder, int position) {
+        VocabFragment vocabFragment = new VocabFragment(data.get(position));
+        FragmentManager fragmentManager = ((MainActivity) context).getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, vocabFragment);
+        fragmentTransaction.commit();
+    }
+
+
+    // Trả về số lượng mục trong danh sách
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.text_view);
+            textView.setOnClickListener(v -> {
+                int position = getAdapterPosition();
+                if (position != RecyclerView.NO_POSITION) {
+                    Toast.makeText(context, data.get(position).def, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+    }
+}
+class Vocab {
+    String term;
+    String def;
+    String ipa;
+
+
+    public Vocab(String term, String def, String ipa) {
+        this.term = term;
+        this.def = def;
+        this.ipa = ipa;
+    }
+
+
+
+}
